2975cbc72f5f0c251af212695507a95915ec93bb
ckomurlu <ckomurlu@216.47.152.109>
2015-02-12 12:52:30 -0600

    Random sampling inference is enhanced. Now, the active inferrer aggregates each set of instances popped from the pool. Previously, it selected candidates from scratch. Next, uncertainty sampling will be implemented.

--- ai/selection_strategy.py
+++ ai/selection_strategy.py
@@ -10,15 +10,17 @@ class RandomStrategy(object):
     def __init__(self, seed=0):
         self.rgen = np.random.RandomState(seed)
     
-    def choices(self, pool, k): 
-        return self.rgen.permutation(pool)[:k]
+    def choices(self, pool, k):
+        permuted_pool = self.rgen.permutation(pool)
+        permuted_pool = map(tuple, permuted_pool)
+        return permuted_pool[:k],permuted_pool[k:]
 
 class UNCSampling(object):
     '''
     This class performs uncertainty sampling based on the model.
     '''
     
-    def choices(self, model, X, pool, k):        
+    def choices(self, model, X, pool, k):
         y_decision = model.decision_function(X[pool])
         uncerts = np.argsort(np.min(np.absolute(y_decision),axis=1))[:k]
         return pool[uncerts]
--- main.py
+++ main.py
@@ -27,6 +27,85 @@ def main():
     print 'use local feature\t', 'Yes' if use_local_features else 'No'
     print 'previous time/current time\t', 'C' if use_current_time else 'P'
     print 'relative features\t', 'binary' if is_relat_feature_binary else 'ordinal' 
+    random_strategy = RandomStrategy()
+    pool = [(i,j) for i in range(test_set.shape[0])
+            for j in range(test_set.shape[1])]
+    iter_count = 10
+    num_trials = 10
+    rate_range = np.arange(0,1.1,0.1)
+    results = np.empty(shape=(num_trials,rate_range.shape[0],3))
+    C = 10
+    for current_trial in range(num_trials):
+        print '\ttrial:',current_trial
+        active_inf_loop(local_classifier_name,
+                    relat_classifier_name,
+                    use_local_features,
+                    use_current_time,
+                    C,
+                    is_relat_feature_binary,
+                    train_set,
+                    test_set, pool,
+                    results,
+                    random_strategy,
+                    rate_range,
+                    current_trial)
+    cPickle.dump(results,open(
+        'dataDays2_3_4-5_6/resultsRandomSamplingLocalOnly_test.pickle','wb'))
+    print 'Duration: ',time() - begin
+    print 'Process ended'
+
+def active_inf_loop(local_classifier_name,
+                    relat_classifier_name,
+                    use_local_features,
+                    use_current_time,
+                    C,
+                    is_relat_feature_binary,
+                    train_set,
+                    test_set, pool,
+                    results,
+                    random_strategy,
+                    rate_range,
+                    current_trial):
+    selectees = []
+    initial_pool_size = len(pool)
+    icaModel = ICAModel(local_classifier_name=local_classifier_name,
+                        relat_classifier_name=relat_classifier_name,
+                        use_local_features=use_local_features,
+                        use_current_time=use_current_time,
+                        relat_classifier_C=C,
+                        is_relat_feature_binary=is_relat_feature_binary)
+    icaModel.fit(train_set)
+    for rate_idx in range(rate_range.shape[0]):
+        if rate_idx == 0:
+            rate_increment = 0
+        else:
+            rate_increment = int(round(initial_pool_size*
+                            (rate_range[rate_idx] - rate_range[rate_idx-1])))
+        print rate_idx, rate_increment
+        current_selectees,pool = random_strategy.choices(pool, rate_increment)
+        selectees += current_selectees
+        evidence_mat = apply_func_to_coords(selectees, test_set.shape)
+        Y_pred = icaModel.predict_by_local_classifiers(test_set)
+        results[current_trial,rate_idx,0] = icaModel.compute_accuracy(
+                        test_set, Y_pred, type_=0,evidence_mat=evidence_mat)
+        results[current_trial,rate_idx,1] = icaModel.compute_accuracy(
+                        test_set, Y_pred, type_=1,evidence_mat=evidence_mat)
+        results[current_trial,rate_idx,2] = icaModel.compute_accuracy(
+                        test_set, Y_pred, type_=2,evidence_mat=evidence_mat)
+def main_old():
+    begin = time()
+    neighborhood_def = all_others_current_time
+    train_set,test_set = convert_time_window_df_randomvar(True,
+                                                          neighborhood_def)
+    use_local_features=True
+    use_current_time=True
+    is_relat_feature_binary=False
+    local_classifier_name = 'svm'
+    relat_classifier_name = 'svm'
+    print 'immediate update\t', 'No'
+    print 'use local feature\t', 'Yes' if use_local_features else 'No'
+    print 'previous time/current time\t', 'C' if use_current_time else 'P'
+    print 'relative features\t', 'binary' if is_relat_feature_binary else 'ordinal' 
     C_values = [10]
 #     C_values = [0.01, 0.02, 0.05, 0.1, 1, 10, 100, 1000, 10000]
     random_strategy = RandomStrategy()
@@ -74,7 +153,6 @@ def main():
         'dataDays2_3_4-5_6/resultsRandomSamplingLocalOnly.pickle','wb'))
     print 'Duration: ',time() - begin
     print 'Process ended'
-
 
 #neighborhood function list
 def independent_back(self_id, sensor_IDs):
