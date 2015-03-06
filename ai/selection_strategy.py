Bugfix: Prediction values were overridden in MAE and MSE computation. Plus, for nodes that refer to none existing neighborhood, the prediction value assigned was inifinigy, for nodes which have these newly assigned nodes in neighborhood, had infinity in their feature, which is a bug. Next, we will try future nodes in the neighborhood.

ai/selection_strategy.py
@@ -7,7 +7,7 @@ Created on Jan 8, 2015
 import numpy as np
 
 class RandomStrategy(object):
-    def __init__(self, seed=0):
+    def __init__(self, seed=1):
         self.rgen = np.random.RandomState(seed)
     
     def choices(self, pool, k):

independent/local_mean_regressor.py
@@ -8,6 +8,7 @@ import numpy as np
 
 from models.ml_model import MLModel
 from utils import constants
+from utils.decorations import deprecated
 
 from sklearn.metrics.metrics import mean_squared_error, mean_absolute_error
 
@@ -40,7 +41,60 @@ class LocalMeanRegressor(MLModel):
                 time_idx = np.where(self.time_values == time_)
                 Y_pred[row_idx,col_idx] = self.mean_mat[row_idx,time_idx]
         return Y_pred
-        
+    
+    def __prepare_prediction_for_evaluation(self,test_set,Y_pred, type_=0,
+                                            evidence_mat=None):
+        assert test_set.shape == Y_pred.shape,'Test set and predicted label'+\
+               ' matrix shape don\'t match'
+#         Y_true = [node.true_label for row in test_set for node in row]
+        if type_ == 0:
+            Y_true = np.vectorize(lambda x: x.true_label)(test_set)
+        else:
+            assert evidence_mat is not None,'The evidence mat is ' + \
+                'not given whereas type_ 1 or 2 accuracy is selected. ' + \
+                'There\'s bug in the flow'
+            if type_ == 1:
+                if np.sum(evidence_mat) == np.prod(evidence_mat.shape):
+                    Y_true = np.array([])
+                    Y_pred = np.array([])
+                else:
+                    Y_true = np.vectorize(lambda x: x.true_label)(test_set[
+                                                        np.invert(evidence_mat)])
+                    Y_pred = Y_pred[np.invert(evidence_mat)]
+            elif type_ == 2:
+                Y_true = np.vectorize(lambda x: x.true_label)(test_set)
+                Y_pred[evidence_mat] = Y_true[evidence_mat]
+            else:
+                raise ValueError('type_ is set a non legit value, it must ' + \
+                                 'be {0,1,2}')
+        legit_indices = np.where(Y_pred != constants.FLOAT_INF)
+        return Y_true,Y_pred,legit_indices    
+    
+    def compute_mean_absolute_error(self,test_set,Y_pred, type_=0, evidence_mat=None):
+        # type_ 0 is the accuracy in which we include everyone with their
+        #     predicted labels
+        # type_ 1 is the accuracy in which we include only the non evidence
+        #     instances
+        # type_ 2 is the one we include again everyone but evidence instances
+        #     participate with true labels.
+        Y_true,Y_pred,legit_indices = self.__prepare_prediction_for_evaluation(
+                    test_set,Y_pred, type_=type_, evidence_mat=evidence_mat)
+        return mean_absolute_error(np.ravel(Y_true[legit_indices]),
+                                  np.ravel(Y_pred[legit_indices]))
+    
+    def compute_mean_squared_error(self,test_set,Y_pred, type_=0, evidence_mat=None):
+        # type_ 0 is the accuracy in which we include everyone with their
+        #     predicted labels
+        # type_ 1 is the accuracy in which we include only the non evidence
+        #     instances
+        # type_ 2 is the one we include again everyone but evidence instances
+        #     participate with true labels.
+        Y_true,Y_pred,legit_indices = self.__prepare_prediction_for_evaluation(
+                    test_set,Y_pred, type_=type_, evidence_mat=evidence_mat)
+        return mean_squared_error(np.ravel(Y_true[legit_indices]),
+                                  np.ravel(Y_pred[legit_indices]))
+    
+    @deprecated
     def compute_accuracy(self,test_set,Y_pred, type_=0, evidence_mat=None):
         # type_ 0 is the accuracy in which we include everyone with their
         #     predicted labels

joint/iterative_regressor.py
@@ -130,6 +130,7 @@ class IRAModel(MLModel):
                         node_ID_dict[neighbor_id],neighbor_time].\
                         true_label
                 else:
+#                     print 'that is not supposed to happen'
                     relat_feature_vector[k] = Y_pred[
                         node_ID_dict[neighbor_id],neighbor_time]
                 
@@ -165,13 +166,21 @@ class IRAModel(MLModel):
                     current_feature_vector = self.generate_relat_feature_vector(
                                                 test_set, i, j, sensor_ID_dict,
                                                 Y_pred, evidence_mat)
-                    if current_feature_vector[0] != constants.FLOAT_INF:
+#                     current_feature_vector = self.generate_relat_feature_vector(
+#                                                 test_set, i, j, sensor_ID_dict)
+                    if current_feature_vector[0] == constants.FLOAT_INF:
+#                         Y_pred_temp[i,j] = constants.FLOAT_INF
+                        Y_pred_temp[i,j] = Y_pred[i,j]  
+                    else:
                         if self.use_local_features:
                             current_feature_vector = np.append(
                                 current_feature_vector,test_set[i,j].\
                                 local_feature_vector)
-                        Y_pred_temp[i,j] = self.relat_regressor[i].predict(
-                            current_feature_vector)
+                        try:
+                            Y_pred_temp[i,j] = self.relat_regressor[i].predict(
+                                current_feature_vector)
+                        except ValueError:
+                            pass
             Y_pred = Y_pred_temp.copy()
         return Y_pred
     
@@ -276,10 +285,12 @@ class IRAModel(MLModel):
         #     instances
         # type_ 2 is the one we include again everyone but evidence instances
         #     participate with true labels.
-        Y_true,Y_pred,legit_indices = self.__prepare_prediction_for_evaluation(
-                    test_set,Y_pred, type_=type_, evidence_mat=evidence_mat)
+        Y_pred_local = copy.deepcopy(Y_pred)
+        Y_true,Y_pred_local,legit_indices = self.__prepare_prediction_for_evaluation(
+                    test_set,Y_pred_local, type_=type_,
+                    evidence_mat=evidence_mat)
         return mean_absolute_error(np.ravel(Y_true[legit_indices]),
-                                  np.ravel(Y_pred[legit_indices]))
+                                  np.ravel(Y_pred_local[legit_indices]))
     
     def compute_mean_squared_error(self,test_set,Y_pred, type_=0, evidence_mat=None):
         # type_ 0 is the accuracy in which we include everyone with their
@@ -288,10 +299,12 @@ class IRAModel(MLModel):
         #     instances
         # type_ 2 is the one we include again everyone but evidence instances
         #     participate with true labels.
-        Y_true,Y_pred,legit_indices = self.__prepare_prediction_for_evaluation(
-                    test_set,Y_pred, type_=type_, evidence_mat=evidence_mat)
+        Y_pred_local = copy.deepcopy(Y_pred)
+        Y_true,Y_pred_local,legit_indices = self.__prepare_prediction_for_evaluation(
+                    test_set,Y_pred_local, type_=type_,
+                    evidence_mat=evidence_mat)
         return mean_squared_error(np.ravel(Y_true[legit_indices]),
-                                  np.ravel(Y_pred[legit_indices]))
+                                  np.ravel(Y_pred_local[legit_indices]))
 
     def compute_accuracy(self, test_set, Y_pred):
         raise NotImplemented('In regression, accuracy cannot be computed.' +

main.py
@@ -9,6 +9,7 @@ from joint.iterative_classifier import ICAModel
 from joint.iterative_regressor import IRAModel
 from ai.selection_strategy import RandomStrategy,UNCSampling
 from utils.node import Neighborhood
+from independent.local_mean_regressor import LocalMeanRegressor
 
 import numpy as np
 import cPickle
@@ -33,16 +34,16 @@ def main_IRA():
 #      = ['lineReg','lasso','ridge','svr']
     regressor_list = list()
 
-#     degree = 1
-#     reg = LinearRegressionExt(degree=degree)
-#     tag = 'Linear Regression'
-#     regressor_list.append((tag,reg))
+    degree = 1
+    reg = LinearRegressionExt(degree=degree)
+    tag = 'Linear Regression'
+    regressor_list.append((tag,reg))
 
-    for kern in ['linear']:
-        for C in [10**pwr for pwr in range(-2,5)]:
-            reg = SVR(kernel=kern,C=C)
-            tag = 'SVR kernel=' + kern + ' C=' + str(C)
-            regressor_list.append((tag,reg))
+#     for kern in ['linear']:
+#         for C in [10**pwr for pwr in range(-2,5)]:
+#             reg = SVR(kernel=kern,C=C)
+#             tag = 'SVR kernel=' + kern + ' C=' + str(C)
+#             regressor_list.append((tag,reg))
 
 #     for alpha in [10**pwr for pwr in range(-7,5)]:
 #         reg = Lasso(alpha=alpha)
@@ -60,12 +61,12 @@ def main_IRA():
                             relat_regressor=regressor,
                             use_local_features=use_local_features)
         iraModel.fit(train_set)
-        Y_pred = iraModel.predict_with_neighbors_true_labels(train_set)
-        print iraModel.compute_mean_absolute_error(train_set, Y_pred, type_=0),'\t',
+#         Y_pred = iraModel.predict_with_neighbors_true_labels(train_set)
+#         print iraModel.compute_mean_absolute_error(train_set, Y_pred, type_=0),'\t',
         Y_pred = iraModel.predict_with_neighbors_true_labels(test_set)
         print iraModel.compute_mean_absolute_error(test_set, Y_pred, type_=0)
     print
-    
+
 def main_regression():
     begin = time()
     neighborhood_def = Neighborhood.itself_previous_others_current
@@ -74,7 +75,6 @@ def main_regression():
     use_local_features=True
     local_regressor_name = 'lasso'
     relat_regressor_name = 'lasso'
-    C = 10
     print 'use local feature\t', 'Yes' if use_local_features else 'No'
     print 'local regression algorithm: ',local_regressor_name
     print 'relational regression algorithm: ',relat_regressor_name
@@ -85,14 +85,21 @@ def main_regression():
     iter_count = 10
     num_trials = 10
     rate_range = np.arange(0,1.1,0.1)
+#     rate_range = np.array([1])
     results = np.empty(shape=(num_trials,rate_range.shape[0],6))
-    degree = 1
-    reg = LinearRegressionExt(degree=degree)
+    alpha = 1.0
+    local_reg = Lasso(alpha=alpha)
+    relat_reg = Lasso(alpha=alpha)
+#     reg = LocalMeanRegressor()
+
+    iraModel = IRAModel(local_regressor=local_reg,
+                        relat_regressor=relat_reg,
+                        use_local_features=use_local_features)
+    iraModel.fit(train_set)
+
     for current_trial in range(num_trials):
         print '\ttrial:',current_trial
-        active_inf_loop_4_regression(reg,
-                    reg,
-                    use_local_features,
+        active_inf_loop_4_regression(iraModel,
                     train_set,
                     test_set, pool,
                     results,
@@ -102,13 +109,22 @@ def main_regression():
                     current_trial)
     print results
     cPickle.dump(results,open(
-        'regressionDataDays2_3_4-5_6/resultsUNCSampling_maxProb.pickle','wb'))
+        'regressionDataDays2_3_4-5_6/resultsRandom_lasso1.0_iter10.pickle','wb'))
     print 'Duration: ',time() - begin
     print 'Process ended'
-    
-def active_inf_loop_4_regression(local_regressor,
-                    relat_regressor,
-                    use_local_features,
+
+# def active_inf_loop_4_regression(local_regressor,
+#                     relat_regressor,
+#                     use_local_features,
+#                     train_set,
+#                     test_set, pool,
+#                     results,
+#                     selection_strategy,
+#                     rate_range,
+#                     iter_count,
+#                     current_trial):
+
+def active_inf_loop_4_regression(iraModel,
                     train_set,
                     test_set, pool,
                     results,
@@ -118,13 +134,9 @@ def active_inf_loop_4_regression(local_regressor,
                     current_trial):
     selectees = []
     initial_pool_size = len(pool)
-    iraModel = IRAModel(local_regressor=local_regressor,
-                        relat_regressor=relat_regressor,
-                        use_local_features=use_local_features)    
-    iraModel.fit(train_set)
-    for rate_idx in range(rate_range.shape[0]):
+    for rate_idx in range(0,rate_range.shape[0]):
         if rate_idx == 0:
-            rate_increment = 0
+            rate_increment = int(round(initial_pool_size*rate_range[rate_idx]))
         else:
             rate_increment = int(round(initial_pool_size*
                             (rate_range[rate_idx] - rate_range[rate_idx-1])))
@@ -155,7 +167,6 @@ def active_inf_loop_4_regression(local_regressor,
                         compute_mean_absolute_error(test_set, Y_pred, type_=2,
                                                    evidence_mat=evidence_mat)
 
-    
 def main_classify():
     begin = time()
     neighborhood_def = Neighborhood.all_others_current_time
@@ -253,9 +264,9 @@ def apply_func_to_coords(coord_list, shape, func=None):
             marked_mat[coord[0],coord[1]] = func(marked_mat[coord[0],coord[1]])
     return marked_mat
 
-
 # main_IRA()
 main_regression()
+
 
 #Old code:
 #                 Y_pred = icaModel.predict(train_set, maxiter=10)