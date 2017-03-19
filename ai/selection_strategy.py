Incremental maximum variance-based selection is implemented. Plus a bugfix: Variance computation in dgbn's computeVar is fixed. I forgot to add the conditional variance to the multiplication of beta square and parents' marginal variances.

ai/selection_strategy.py
@@ -53,6 +53,8 @@ class StrategyFactory(object):
             selection_strategy = VarianceBased()
         elif 'varianceBased2' == strategy_name:
             selection_strategy = VarianceBased2()
+        elif 'incrementalVariance' == strategy_name:
+            selection_strategy = IncrementalVarianceBased()
         else:
             raise ValueError('Unknown strategy choice. Please double check selection strategy name in ' +
                              'utils.properties.')
@@ -244,8 +246,8 @@ class VarianceBased2(AbstractSelectionStrategy):
     def __init__(self, **kwargs):
         self.mostRecentSelectees = None
 
-    def choices(self, count_selectees, t, curEvidMat, predictionModel, **kwargs):
-        varianceList = predictionModel.computeVar(curEvidMat)
+    def choices(self, count_selectees, evidMat, t, predictionModel, **kwargs):
+        varianceList = predictionModel.computeVar(evidMat[:, :t + 1])
         sortedIds = np.argsort(varianceList)
         sortedIds = sortedIds[::-1]
         self.mostRecentSelectees = sortedIds[:count_selectees]
@@ -254,6 +256,23 @@ class VarianceBased2(AbstractSelectionStrategy):
     def __str__(self):
         return 'most recent selectees: ' + str(self.mostRecentSelectees)
 
+
+class IncrementalVarianceBased(AbstractSelectionStrategy):
+    def __init__(self, **kwargs):
+        self.mostRecentSelectees = None
+
+    def choices(self, count_selectees, evidMat, t, predictionModel, **kwargs):
+        tempEvidMat = evidMat[:, :t + 1]
+        self.mostRecentSelectees = list()
+        for i in range(count_selectees):
+            varianceList = predictionModel.computeVar(tempEvidMat)
+            rvMaxVar = np.argmax(varianceList)
+            self.mostRecentSelectees.append(rvMaxVar)
+            tempEvidMat[rvMaxVar, t] = 1
+        return self.mostRecentSelectees
+
+    def __str__(self):
+        return 'most recent selectees: ' + str(self.mostRecentSelectees)
 
 class UNCSampling(object):
     '''

independent/gaussian_process.py
@@ -34,7 +34,8 @@ class GaussianProcessLocal(MLRegModel):
             Xtrain = np.vectorize(lambda x: x.local_feature_vector)(train_mat)
             ytrain = np.vectorize(lambda x: x.true_label)(train_mat)
 
-            ytrain_split = np.split(ytrain, 59, axis=1)
+            ytrain_split = np.split(ytrain, 3, axis=1)  # Intel data
+            # ytrain_split = np.split(ytrain, 59, axis=1)  #Wunderground data
             ytrain_split_array = np.array(ytrain_split)
             ytrain_mean = np.mean(ytrain_split_array, axis=0)
 
@@ -43,8 +44,19 @@ class GaussianProcessLocal(MLRegModel):
             for row in range(self.rvCount):
                 # self.gpmat[row] = GaussianProcess(corr='cubic', theta0=1e-2, thetaL=1e-4, thetaU=1e-1,
                 #                                   random_start=100)
-                self.gpmat[row] = GaussianProcess(corr='linear', theta0=1e-2, thetaL=1e-4, thetaU=1e-1,random_start=100)
-                self.gpmat[row].fit(Xtrain[row, :4].reshape(-1, 1), ytrain_mean[row])
+                self.gpmat[row] = GaussianProcess(corr='linear', theta0=1e-2, thetaL=1e-4, thetaU=1e-1,
+                                                           random_start=100)
+                self.gpmat[row].fit(Xtrain[row, :48].reshape(-1, 1), ytrain_mean[row])  # Intel data
+                # self.gpmat[row].fit(Xtrain[row, :4].reshape(-1, 1), ytrain_mean[row])  # Wunderground data
+
+    def computeVar(self, evidMat):
+        raise NotImplementedError
+        T = evidMat.shape[1]
+        varMat = np.zeros(shape=(self.rvCount, T))
+        for rv in range(self.rvCount):
+            for t in range(T):
+                self.gpmat[rvid].predict
+
 
     def predict(self, test_mat, evid_mat, **kwargs):
         Xtest = np.vectorize(lambda x: x.local_feature_vector)(test_mat)

joint/gaussian_DBN.py
@@ -362,19 +362,15 @@ class GaussianDBN(MLRegModel):
     def computeVar(self, evidMat):
         T = evidMat.shape[1]
         varMat = np.empty(shape=(self.rvCount,T), dtype=np.float_)
-        if evidMat[self.sortedids[0], 0]:
-            varMat[self.sortedids[0], 0] = 0
-        else:
-            varMat[self.sortedids[0], 0] = self.cpdParams[self.sortedids[0]][0][2]
-        for sensor in self.sortedids[1:]:
+        # varMat[self.sortedids[0], 0] = 0 if evidMat[self.sortedids[0], 0] else self.cpdParams[self.sortedids[0]][0][2]
+        for sensor in self.sortedids:  # [1:]:
             if evidMat[sensor, 0]:
                 varMat[sensor, 0] = 0
             else:
-                currentVar = self.cpdParams[sensor][1][2]
-                # varVec = np.empty(shape=len(self.parentDict[sensor]))
+                currentVar = self.cpdParams[sensor][0][2]
                 currentTimeParents = self.parentDict[sensor][:-1]
                 varVec = varMat[currentTimeParents, 0]
-                varMat[sensor,0] = np.dot(varVec, (self.cpdParams[sensor][1][1][:-1] ** 2))
+                varMat[sensor,0] = np.dot(varVec, (self.cpdParams[sensor][0][1] ** 2)) + currentVar
         for t in range(1,T):
             for sensor in self.sortedids:
                 if evidMat[sensor, t]:
@@ -386,8 +382,7 @@ class GaussianDBN(MLRegModel):
                     varVec[:-1] = varMat[currentTimeParents, t]
                     previousTimeParent = self.parentDict[sensor][-1]
                     varVec[-1] = varMat[previousTimeParent - self.rvCount, t-1]
-                    # varMat[sensor,t] = varVec * (self.cpdParams[sensor][1][1] ** 2)
-                    varMat[sensor,t] = np.dot(varVec, (self.cpdParams[sensor][1][1] ** 2))
+                    varMat[sensor,t] = np.dot(varVec, (self.cpdParams[sensor][1][1] ** 2)) + currentVar
         return varMat[:, -1]
 
     def predict_incorrect(self, testset, evidence_mat=None):

models/ml_reg_model.py
@@ -52,7 +52,9 @@ class MLRegModel(MLModel):
                                  'be {0,1,2}')
         legit_indices = np.where(Y_pred != constants.FLOAT_INF)
         return Y_true,Y_pred,legit_indices
-        
+
+    def computeVar(self):
+        raise NotImplementedError
 
     def compute_mean_absolute_error(self,test_set,Y_pred, type_=0, evidence_mat=None):
         # type_ 0 is the accuracy in which we include everyone with their

utils/properties.py
@@ -13,7 +13,7 @@ ts = time.time()
 # temperature data
 temperature_k2_bin10_topology_ParentChildDictPath = 'C:/Users/ckomurlu/Documents/workbench/experiments/20150911/' + \
                                       'parentChildDicts_k2_bin10.pkl'
-temperature_k2_bin5_topology_ParentChildDictPath = 'C:/Users/ckomurlu/Documents/workbench/experiments/20151023/' + \
+temperature_k2_bin5_topology_ParentChildDictPath = 'C:/Users/ckomurlu/Documents/workbench/data/intelResearchLab/' + \
                                      'parentChildDicts_k2_bin5.pkl'
 
 # humidity
@@ -36,7 +36,7 @@ wground_cdwide_k2_bin5_topology_ParentChildDictPath = 'C:/Users/ckomurlu/Documen
 # outputDirPath = 'C:/Users/ckomurlu/Documents/workbench/experiments/20151029/5_GP/'
 # outputDirPath = 'C:/Users/ckomurlu/Documents/workbench/experiments/20151121/temperature/GP/SW/'
 # outputDirPath = 'C:/Users/ckomurlu/Documents/workbench/experiments/20161227/temperature/DGBN/SW/'
-outputDirPath = 'C:/Users/ckomurlu/Documents/workbench/experiments/20170120/countrywide/DGBN/RND/'
+outputDirPath = 'C:/Users/ckomurlu/Documents/workbench/experiments/20170227/temperature/DGBN/VAR2/'
 timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d_%H%M')
 numParallelThreads = 10
 
@@ -48,22 +48,23 @@ mh_startupWidth = 5.0
 tWin = 12
 timeSpan = 12
 obsrateList = np.arange(0.0, 0.7, 0.1)
-numTrials = 5
+numTrials = 1
 
-selectionStrategy = 'randomStrategy2'
+# selectionStrategy = 'randomStrategy2'
 # selectionStrategy = 'slidingWindow'
 # selectionStrategy = 'impactBased'
 # selectionStrategy = 'netImpactBased'
 # selectionStrategy = 'varianceBased'
-# selectionStrategy = 'varianceBased2'
+selectionStrategy = 'varianceBased2'
+# selectionStrategy = 'incrementalVariance'
 
 dbn_topology = 'k2_bin5' #, 'k2_bin10', 'mst', 'mst_enriched', 'imt'
 
-# data = 'temperature'
+data = 'temperature'
 # data = 'humidity'
 # data = 'temperature+humidity'
 # data = 'wunderground-IL'
-data = 'wunderground-cwide'
+# data = 'wunderground-cwide'
 
 # prediction_model = 'gp'
 # prediction_model = 'kf'